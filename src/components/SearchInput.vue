f0c806792d58f272e8e99c5ec2f848ba34c295d3
annaskuza7 <anna.skuza7@gmail.com>
2022-01-13 12:55:24 +0100

    Add basic hero image view

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="searchWrapper">
+    <input id="search" name="search" type="text" v-model="searchValue" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchInput",
+};
+</script>
+
+<style lang="scss" scoped>
+.searchWrapper {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+
+  input {
+    height: 30px;
+    background: none;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
